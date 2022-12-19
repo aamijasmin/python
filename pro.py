fe03a0f89b1016fda30cc96d9bad5fba6fe7efa8
aamijasmin <jesminp5486@gmail.com>
2022-12-19 16:57:05 +0530

    Learning Python  🐍

---
+++ pro.py
@@ -0,0 +1,33 @@
+mobs = ['vivo', 'oppo', 'samsung', 'lava', 'iphon', 'realme', 'redme']
+
+
+fav_mobs = ['vivo', 'oppo', 'realme']
+
+mobs[2] = "Asus"
+print(fav_mobs)
+for f in fav_mobs:
+     if f in mobs:
+         print("mob : "+ f +"is available ")
+     else:
+
+         print("mob : "+ f +"is not available ")
+
+
+
+# mobs = ['vivo', 'oppo', 'samsung', 'lava', 'iphon', 'realme', 'redme']
+# for f in fruits:
+    # print("fruits", f.title())
+
+flowrs = ['lily', 'rose', 'daliya', 'lotus', 'jasmine']   
+
+for f in flowrs:
+    print("flower", f.upper())
+    
+fruits = ['mango', 'apple', 'banana', 'grape', 'orange']
+fruits[2] = "kiwi"
+for j in fruits:
+    print(j)
+
+name = ['jasmin', 'anupam', 'sutapa', 'riya', 'sanju', 'rosni', 'sonali', 'susmita', 'rahul', 'mohasin', 'moha', 'jonaki', 'juliet', 'raaj','priya', 'harimahan', ' haridas', 'haripal', 'suman', 'joya', 'prantik', 'kartik', 'nayra', 'arif', 'souvik', 'ripon', 'puspa', 'allu arjun', 'ram', 'xukai', 'sunni', 'nobita', 'sunio', 'sinchain', 'gyan', 'arman', 'oggy', 'jack', 'paplu', 'dhaplu', 'jhaplu', 'sizuka', 'priti', 'ruhi', 'kenichi', 'ninja hattori', 'sisumanu', 'shinzo', 'bob', 'kutta']
+for k in name:
+    print("hiiii kii khobor ", k.title())
